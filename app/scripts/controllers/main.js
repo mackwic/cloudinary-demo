924983ef2d91c7ad323e080c5c04003eaefd8285
Thomas Wickham <mackwic@gmail.com>
2013-05-27 13:40:43 +0200

    Adding an exception handler because looking at the console log is too painful

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,43 @@
 
 var module = angular.module('practiceApp');
 
+
 module.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+});
+
+var ExceptionCtrl = function($scope) {
+    $scope.exp = [];
+
+    $scope.addException = function(e, cause) {
+        var str = 
+            ((e.fileName !== undefined) ? '@' + e.fileName : '')
+            + ((e.lineNumber !== undefined) ? ':' + e.lineNumber : '')
+            + ' ' + e.message
+            + ((cause !== undefined) ? '\ncause: ' + cause : '');
+
+        $scope.exp.push(str);
+    };
+};
+
+var ExceptionProvider = {
+
+    $exceptionHandler: function() {
+        var handler = function (e, cause) {
+            var elt = document.getElementById('excp');
+            var $scope = angular.element(elt).scope();
+            $scope.addException(e, cause)
+        };
+
+        this.$get = function() {
+            return handler;
+        };
+    }
+};
+module.provider(ExceptionProvider);
+
+module.controller('ExceptionCtrl', ExceptionCtrl);
--- app/scripts/controllers/upload.js
+++ app/scripts/controllers/upload.js
@@ -2,8 +2,22 @@
 
 var module = angular.module('practiceApp');
 
-module.controller('UploadCtrl', function($scope) {    
-    $scope.files = [];
+var UploadCtrl = function($scope) {
+    /*
+        var ctrl = function($scope) {
+            $scope.files = [];
 
-    $scope.submitMsg = 'Choose which image to send';
-  });
+            $scope.submitMsg = 'Choose which image to send';
+        },
+
+        var setFiles = function(elt) {
+                    $scope.$apply(function() {
+                    $scope.files = elt.files;
+                };
+            }
+    };
+    */
+};
+
+module.controller('UploadCtrl', UploadCtrl.ctrl);
+
